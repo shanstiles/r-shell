5d8f3cc9ca46f7c393691719a1008e993c67042b
Shannon Stiles <sstiles@wisc.edu>
2019-11-22 11:16:33 -0600

    Edited render-rmd script for multiple input arguments.  Now able to create multiple reports from different files that we indicated in the terminal command with different file names through a loop.

--- render-rmd.R
+++ render-rmd.R
@@ -1,7 +1,17 @@
 library("rmarkdown")
+library("stringr")
 
 args <- commandArgs(trailingOnly = TRUE)
 
-render("surveys-report.Rmd", 
-       output_dir = "reports",
-       params = list(file = args))
+for(file in args)
+{
+  out_name <- str_split_fixed(string = file,
+                               pattern = "[/|.]",
+                               n = 3)
+  out_name <- str_c(out_name[2], '.html')
+  
+  render("surveys-report.Rmd", 
+         output_file = out_name,
+         output_dir = "reports",
+         params = list(file = file))
+}
